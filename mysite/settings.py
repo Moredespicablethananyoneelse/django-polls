aeb9698a0af56de261e1aab7f2ae84a951d06dd3
Moredespicablethananyoneelse <donotblieveit@outlook.com>
2025-04-16 11:03:23 +0800

    before Conclusion

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:1337"]
