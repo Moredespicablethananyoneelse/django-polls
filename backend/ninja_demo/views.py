ca0b9232f6935c29e9ae743db5cb67000618654f
Moredespicablethananyoneelse <donotblieveit@outlook.com>
2025-05-13 19:43:44 +0800

    add backend sub directory

---
+++ backend/ninja_demo/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from ninja import NinjaAPI
+from ninja.security import django_auth
+
+from allauth.headless.contrib.ninja.security import x_session_token_auth
+
+
+api = NinjaAPI()
+
+
+@api.get("/add", auth=[django_auth, x_session_token_auth])
+def add(request, x: float, y: float):
+    return {"result": x + y}
